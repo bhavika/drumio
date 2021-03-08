06697b84e2ba19b443a95983ba24ffc9c0b06f49
bhavika <bhavicka.7992@gmail.com>
2021-03-07 21:52:12 -0500

    Set up terraform

---
+++ tf/eb_app.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = var.region
+  shared_credentials_file = "$HOME/.aws/credentials"
+  profile = "default"
+}
+
+resource "aws_iam_role" "beanstalk_service" {
+  name = "beanstalk-service-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_elastic_beanstalk_application" "drumio" {
+  name = "drumio-${var.env}"
+  description = "Source separation in your browser"
+  tags = {
+    ProjectName = "drumio"
+  }
+  appversion_lifecycle {
+    service_role = aws_iam_role.beanstalk_service.arn
+    max_count = 128
+    delete_source_from_s3 = false
+  }
+}
+
+resource "aws_elastic_beanstalk_environment" "drumio-env" {
+  application = aws_elastic_beanstalk_application.drumio.name
+  name = "drumio-env-${var.env}"
+  solution_stack_name = "64bit Amazon Linux 2 v3.2.0 running Python 3.7"
+  tags = {
+    ProjectName = "drumio"
+  }
+}
